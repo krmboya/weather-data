5d762ba5dcde1abecf12f2fa59c6ece3a12db203
Kenny Rachuonyo <krmboya@gmail.com>
2016-12-15 13:56:28 +0300

    minor ommission

--- max_spread.py
+++ max_spread.py
@@ -80,7 +80,7 @@ def main(filename):
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
-        sys.stderr.write("usage: {0} <filename>\n")
+        sys.stderr.write("usage: python {0} <filename>\n".format(sys.argv[0]))
         sys.exit(1)
 
     filename = sys.argv[1]
